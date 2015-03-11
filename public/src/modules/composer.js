d0f07405413921efdac058fda45441ba7eb96c50
psychobunny <psycho.bunny@hotmail.com>
2015-03-11 16:34:40 -0400

    return composing state to normal after discard/posting

--- public/src/modules/composer.js
+++ public/src/modules/composer.js
@@ -550,7 +550,8 @@ define('composer', [
 			$('body').css({'margin-bottom': 0});
 			$('[data-action="post"]').removeAttr('disabled');
 
-			app.toggleNavbar(true);
+			
+			$('html').removeClass('composing mobile');
 		}
 	}
 
